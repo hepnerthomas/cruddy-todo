69ae34aab75500f555a674110dc884476999619b
Thomas Hepner <hepner.thomas@gmail.com>
2022-08-09 10:59:12 -0700

    feat: refactor create and readAll functions with all tests passing.

--- datastore/counter.js
+++ datastore/counter.js
@@ -15,7 +15,7 @@ var counter = 0;
 // Wikipedia entry on Leading Zeros and check out some of code links:
 // https://www.google.com/search?q=what+is+a+zero+padded+number%3F
 
-const zeroPaddedNumber = (num) => {
+exports.zeroPaddedNumber = (num) => {
   return sprintf('%05d', num);
 };
 
@@ -30,7 +30,7 @@ const readCounter = (callback) => {
 };
 
 const writeCounter = (count, callback) => {
-  var counterString = zeroPaddedNumber(count);
+  var counterString = exports.zeroPaddedNumber(count);
   fs.writeFile(exports.counterFile, counterString, (err) => {
     if (err) {
       throw ('error writing counter');
--- datastore/index.js
+++ datastore/index.js
@@ -4,7 +4,7 @@ const _ = require('underscore');
 const counter = require('./counter');
 
 // Thomas Notes:
-// replace items with a datastore, dataDir
+// replace items with a datastore, datastore/data/
 // dataDir = index.js/id.txt
 // const count = require("./datastore/counter.txt");
 
@@ -13,27 +13,47 @@ var items = {};
 // Public API - Fix these CRUD functions ///////////////////////////////////////
 
 exports.create = (text, callback) => {
-  var id = counter.getNextUniqueId();
-  items[id] = text;
-  callback(null, { id, text });
+  counter.getNextUniqueId((err, id) => {
+    if (err) {
+      throw ('create error');
+    }
+    newTodoListDataFile = id + '.txt';
+    newTodoListDataFileDir = path.join(exports.dataDir, newTodoListDataFile);
+    // console.log(newTodoListDataFileDir);
+
+    fs.writeFile(newTodoListDataFileDir, text, (err) => {
+      if (err) {
+        throw ('Error in writing new todo item');
+      } else {
+        // callback(null, text);
+        callback(null, { id, text });
+      }
+    });
+  });
+
+
 };
 
-// Thomas Notes:
-// replace items using fs
-// use fs.writeFile() to write text to with the id in the name of the file
-  // fs.writeFile(exports.counterFile, counterString, (err) => {
-  //   if (err) {
-  //     throw ('error writing counter');
-  //   } else {
-  //     callback(null, counterString);
-  //   }
-  // });
-
 exports.readAll = (callback) => {
-  var data = _.map(items, (text, id) => {
-    return { id, text };
+  // identify the path to the chosen folder (exports.dataDir)
+  // fs.readdir (directoryPath, (err, files))
+  fs.readdir(exports.dataDir, (err, files) => {
+    // check err
+    if (err) {
+      throw ('Failed to read files from data directory.');
+    }
+    // files.forEach, create an object that contains key id, key text
+    // (suggestion: we may need to log out (file) to see what's in it)
+    // where id is the name of the file (without .txt)
+    // and text is also the name of the file (without .txt)
+    var data = _.map(files, (fileName) => {
+      // console.log({ id: id, text: id });
+      let id = fileName.replace('.txt', '');
+      return { id: id, text: id };
+    });
+    callback(null, data);
   });
-  callback(null, data);
+
 };
 
 exports.readOne = (id, callback) => {
